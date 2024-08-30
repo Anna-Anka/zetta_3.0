b1ab18d26bc92f5679fd60fc936177f1c361c629
Anna-Anka <anjbekrenewa@gmail.com>
2024-08-30 23:02:54 +0300

    start blog block

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -1,6 +1,7 @@
 import './sliders/_reviews-slider.js'
 import './sliders/_video-reviews-slider.js'
 import './sliders/_completed-projects-sliders.js'
+import './sliders/_blog-slider.js'
 import { showTooltipInProductCard } from './_product-card-tooltip.js'
 
 showTooltipInProductCard();
---
+++ src/js/project/sliders/_blog-slider.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+Swiper.use([Pagination, Navigation]);
+
+if (document.querySelector('.blog-block__swiper')) {
+    new Swiper('.blog-block__swiper', {
+        loop: true,
+        speed: 800,
+
+        navigation: {
+            nextEl: '.blog-block__button--next',
+            prevEl: '.blog-block__button--prev',
+        },
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerGroup: 3,
+                slidesPerView: 3,
+            },
+
+            576: {
+                slidesPerGroup: 2,
+                slidesPerView: 2,
+            },
+
+            0: {
+                slidesPerGroup: 1.2,
+                slidesPerView: 1,
+            },
+        },
+    });
+}
