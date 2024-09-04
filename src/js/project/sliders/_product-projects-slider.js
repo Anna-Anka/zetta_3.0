1e362bd3a5fe3b753777e82b2917231564ee688c
Anna-Anka <anjbekrenewa@gmail.com>
2024-09-04 19:03:33 +0300

    add product-projects

---
+++ src/js/project/sliders/_product-projects-slider.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+Swiper.use([Pagination, Navigation]);
+
+if (document.querySelector('.product-projects__swiper')) {
+    new Swiper('.product-projects__swiper', {
+        loop: true,
+        speed: 800,
+        spaceBetween: 20,
+
+        navigation: {
+            nextEl: '.product-projects__button--next',
+            prevEl: '.product-projects__button--prev',
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerGroup: 2,
+                slidesPerView: 2,
+                spaceBetween: 20,
+
+                pagination: {
+                    el: '.product-projects__fraction',
+                    type: 'fraction',
+                    formatFractionCurrent: function (number) {
+                        return number > 9 ? number : ('0' + number);
+                    },
+                    formatFractionTotal: function (number) {
+                        return number > 9 ? number : ('0' + number);
+                    },
+                    renderFraction: function (currentClass, totalClass) {
+                        return `<span class="${currentClass}"></span> / <span class="${totalClass}"></span>`
+                    }
+                },
+            },
+            0: {
+                slidesPerGroup: 1,
+                slidesPerView: 1,
+                spaceBetween: 10,
+
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            },
+        },
+    });
+}
