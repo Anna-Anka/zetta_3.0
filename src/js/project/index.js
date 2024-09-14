94c47a5c7ee7c3906d19cefb0d23a37ef700ef8f
Anna-Anka <anjbekrenewa@gmail.com>
2024-09-14 20:36:17 +0300

    refactor

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -1,16 +1,16 @@
-import './sliders/_hero-home-slider.js'
-import './sliders/_reviews-slider.js'
-import './sliders/_video-reviews-slider.js'
-import './sliders/_completed-projects-sliders.js'
-import './sliders/_blog-slider.js'
-import './sliders/_product-projects-slider.js'
-import './sliders/_other-solutions-slider.js'
-import './sliders/_product-about-slider.js'
-import { fixedHeader } from './_fixed-header.js'
-import { manageMenuCategories } from './_menu-categories.js'
-import { changeColorSideMenu } from './_side-menu.js'
-import { changeBlogImages } from './_blog-images.js'
-import { createConveniencePoints, arrangeConveniencePoints } from './_convenience.js'
+import './sliders/_hero-home-slider.js';
+import './sliders/_reviews-slider.js';
+import './sliders/_video-reviews-slider.js';
+import './sliders/_completed-projects-sliders.js';
+import './sliders/_blog-slider.js';
+import './sliders/_product-projects-slider.js';
+import './sliders/_other-solutions-slider.js';
+import './sliders/_product-about-slider.js';
+import { fixedHeader } from './_fixed-header.js';
+import { manageMenuCategories } from './_menu-categories.js';
+import { changeColorSideMenu } from './_side-menu.js';
+import { changeBlogImages } from './_blog-images.js';
+import { createConveniencePoints, arrangeConveniencePoints } from './_convenience.js';
 
 fixedHeader();
 manageMenuCategories();
